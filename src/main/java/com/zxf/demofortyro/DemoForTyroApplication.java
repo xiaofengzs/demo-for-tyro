5e78becde2173dd000e57f51d5113e7fe34aedbe
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-05-06 11:07:38 +0800

    [xiaofeng.zhi]: init project

---
+++ src/main/java/com/zxf/demofortyro/DemoForTyroApplication.java
@@ -0,0 +1,13 @@
+package com.zxf.demofortyro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForTyroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForTyroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zxf/demofortyro/DemoForTyroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxf.demofortyro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForTyroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
