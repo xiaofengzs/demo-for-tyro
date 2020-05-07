134b3207916b542539604bd4d530df85a131e0b0
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-05-07 13:17:01 +0800

    [xiaofeng.zhi]: modify build docker shell

--- build-docker.sh
+++ build-docker.sh
@@ -6,8 +6,5 @@ echo "build docker image"
 docker build -t="${DOCKER_IMAGE}" ./
 docker tag ${DOCKER_IMAGE} ${DOCKER_IMAGE}:lastest
 
-echo "run docker image"
-docker run ${DOCKER_IMAGE}
-
 echo "push docker image"
 docker push ${DOCKER_IMAGE}:lastest
