6184011009c87930a72b7776e4bbb8907be808b6
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-06-03 15:07:07 +0800

    add grafana and promthues

--- src/main/java/com/zxf/demofortyro/DemoForTyroApplication.java
+++ src/main/java/com/zxf/demofortyro/DemoForTyroApplication.java
@@ -1,13 +1,40 @@
 package com.zxf.demofortyro;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.Histogram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoForTyroApplication {
+
+	// Define a counter metric for /prometheus
+	static final Counter requests = Counter.build()
+			.name("requests_total").help("Total number of requests.").register();
+	// Define a histogram metric for /prometheus
+	static final Histogram requestLatency = Histogram.build()
+			.name("requests_latency_seconds").help("Request latency in seconds.").register();
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoForTyroApplication.class, args);
+	}
+
+	@GetMapping("/")
+	String home() {
+		// Increase the counter metric
+		requests.inc();
+		// Start the histogram timer
+		Histogram.Timer requestTimer = requestLatency.startTimer();
+		try {
+			return "Hello World!";
+		} finally {
+			// Stop the histogram timer
+			requestTimer.observeDuration();
+		}
 	}
 
 }
--- src/main/java/com/zxf/demofortyro/HelloController.java
+++ src/main/java/com/zxf/demofortyro/HelloController.java
@@ -1,16 +1,26 @@
 package com.zxf.demofortyro;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.Histogram;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @PostMapping("/api/greeting/say/{personname}")
     public ResponseEntity<String> sayHi(@PathVariable String personname) {
         System.out.println(personname);
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("hi, " + personname);
+    }
+
+    @GetMapping("/api/greeting/bye/{personname}")
+    public ResponseEntity<String> sayBye(@PathVariable String personname) {
+        System.out.println(personname);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("see you agian, " + personname);
     }
 }
